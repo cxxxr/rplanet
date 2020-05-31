3426eb5f9e89d4d5d0887d37b677da73b51ffff0
cxxxr <g23tlm@gmail.com>
2020-05-31 16:00:09 +0900

    fix

--- valtan/webpack.config.js
+++ valtan/webpack.config.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 module.exports = {
   mode: 'none',
-  entry: './rplanet.js',
+  entry: './.valtan-cache/rplanet.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, '../public/assets/dist')
